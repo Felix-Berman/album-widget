3baf0a3664baba4a31d785354ae5f4d0fb8907b3
Felix Berman <felix@berman.id.au>
2022-11-02 18:48:59 +1100

    Remove Scriptable icon variables

--- album-widget.js
+++ album-widget.js
@@ -1,8 +1,3 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: green; icon-glyph: magic;
-
-
 // fill in user specific data
 const apiKey = ''	// last.fm api key
 const user = '' // lastfm user name
